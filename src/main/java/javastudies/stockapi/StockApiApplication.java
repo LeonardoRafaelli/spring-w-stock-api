7a07c1daa99e09ea076436d99872f03e4b5acdf8
leonardorafaelli <leonardorafaelli@gmail.com>
2024-05-12 00:24:36 +0200

    removes boilplate code

--- src/main/java/javastudies/stockapi/StockApiApplication.java
+++ src/main/java/javastudies/stockapi/StockApiApplication.java
@@ -12,10 +12,7 @@ import java.net.http.HttpResponse;
 @SpringBootApplication
 @EnableConfigurationProperties(AlphaVantageConfig.class)
 public class StockApiApplication {
-//	private static final String BASE_URL = "https://www.alphavantage.co/query?";
-//	private static final String FUNCTION = "TIME_SERIES_MONTHLY";
-//	private static final String SYMBOL = "AAPL";
-//	private static final String API_KEY = "UV0T6HSE3GZFLN42";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StockApiApplication.class, args);
 
