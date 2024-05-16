bdb714e494e6038fcab7608d4133b2e75c881244
leonardorafaelli <leonardorafaelli@gmail.com>
2024-05-16 21:56:18 +0200

    feat: implement PRG pattern for search stock form

--- src/main/java/javastudies/stockapi/controller/UserController.java
+++ src/main/java/javastudies/stockapi/controller/UserController.java
@@ -2,6 +2,7 @@ package javastudies.stockapi.controller;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import jakarta.servlet.http.HttpServletRequest;
 import javastudies.stockapi.DTO.GlobalQuoteDTO;
 import javastudies.stockapi.DTO.StockSearchDTO;
 import javastudies.stockapi.api.AlphaVantageConfig;
@@ -13,12 +14,18 @@ import javastudies.stockapi.model.Account;
 import javastudies.stockapi.model.User;
 import javastudies.stockapi.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/my-stocks")
@@ -29,18 +36,29 @@ public class UserController {
     private final UserServiceImpl userService;
 
     @RequestMapping("/home")
-    public String getGreetings(Model m, @AuthenticationPrincipal CustomUserDetails userDtls){
+    public String getGreetings(
+            HttpServletRequest request,
+            Model m,
+            @AuthenticationPrincipal CustomUserDetails userDtls
+    ){
+        Map<String, ?> inputFlashMap = RequestContextUtils.getInputFlashMap(request);
+        if(inputFlashMap != null) {
+            GlobalQuoteDTO globalQuoteDTO = (GlobalQuoteDTO) inputFlashMap.get("stock");
+            m.addAttribute("stock", globalQuoteDTO.getGlobalQuote());
+        }
         m.addAttribute("user", userDtls.getUser());
         m.addAttribute("userAccount", userDtls.getUser().getAccount());
         return "home";
     }
 
     @PostMapping("/get-stock-info")
-    public String getStockInfo(Model m, @ModelAttribute StockSearchDTO stockSearch) {
+    public RedirectView getStockInfo(
+            Model m,
+            @ModelAttribute StockSearchDTO stockSearch,
+            RedirectAttributes redirectAttributes
+    ) {
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingStrategy(new CustomFieldNamingStrategy())
-                .create();
+        Gson gson = new Gson();
 
         String apiResponse = FetchAlphaApi.builder()
         .apiFunction(AlphaVantageFunctionType.GLOBAL_QUOTE)
@@ -53,8 +71,14 @@ public class UserController {
         GlobalQuoteDTO globalQuoteDTO = gson.fromJson(apiResponse, GlobalQuoteDTO.class);
         System.out.println(STR."Global Quote DTO = \{globalQuoteDTO}");
 
-        m.addAttribute("stock", globalQuoteDTO.getGlobalQuote());
+        redirectAttributes.addFlashAttribute("stock", globalQuoteDTO);
 
-        return "home";
+        return new RedirectView("/my-stocks/home", true);
     };
+
+    @PostMapping("/add-money")
+    public String addMoneyToAccount(){
+        System.out.println("Chegou em Add Money");
+        return "redirect:/home";
+    }
 }
